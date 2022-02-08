4cf0d97ee10b383803291b472e48c620c70dde44
maxmusiichuk <musiichuk646@gmail.com>
2022-02-08 11:45:08 +0200

    Add styles for input fielsd & title

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="main-input--wrapper">
-    <input type="text" v-model="inputValue" @keyup.enter="onEnterKey">
+    <div class="header-title">TODO LIST</div>
+    <input class="input-field" type="text" v-model="inputValue" @keyup.enter="onEnterKey">
   </div>
 </template>
 
@@ -45,5 +46,23 @@ export default {
 </script>
 
 <style scoped>
-
+.input-field{
+  margin-top: 20px;
+  width: 350px;
+  height: 40px;
+  border: 3px solid #83a4c4;
+  border-radius: 12px;
+  font-family: 'Work Sans', sans-serif;
+  font-weight: 400;
+  font-size: 22px;
+  color: #81a9d4;
+  padding: 10px;
+}
+.header-title{
+  font-family: 'Work Sans', sans-serif;
+  font-weight: 700;
+  font-style: italic;
+  font-size: 50px;
+  color: #81a9d4;
+}
 </style>
