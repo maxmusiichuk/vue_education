6fc56776ff050705b254fae91ec3f7c43166925f
maxmusiichuk <musiichuk646@gmail.com>
2022-02-08 12:20:01 +0200

    Add styles for list

--- src/components/sides/LeftSide.vue
+++ src/components/sides/LeftSide.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="side-wrapper">
-    <h1>Need to do</h1>
-    <ul v-for="(item,index) in tasksOutput" :key="item.id">
-      <li @click="removeItem(index)"><p :class="['task-id-'+item.id]" style="text-align: left">{{ item.name }} -
-        {{ item.id }}</p></li>
+<!--    <h1>Need to do</h1>-->
+    <ul class="task-list-wrapper">
+<!--      <li v-for="(item,index) in tasksOutput" :key="item.id" @click="removeItem(index)">-->
+      <li class="task-list--item" v-for="(item,index) in tasksOutput" :key="item.id" >
+        <span class="checkbox-item"><input class="checkbox-switch" type="checkbox" @click="removeItem(item)" ></span>
+        <p :class="['task-id', 'dynamic-class-'+item.id]" style="text-align: left">
+        {{ item.name }}. Status: {{ item.doneStatus }}</p>
+      </li>
     </ul>
   </div>
 </template>
@@ -39,7 +43,8 @@ export default {
   },
   methods: {
     removeItem(itemId) {
-      this.$store.commit("main/removeTask", itemId);
+      itemId.doneStatus = !itemId.doneStatus;
+      // this.$store.commit("main/removeTask", itemId);
     },
     consoleItem(item) {
       console.log(item)
@@ -49,5 +54,40 @@ export default {
 </script>
 
 <style scoped>
-
+.task-id{
+  padding-left: 15px;
+  font-family: 'Work Sans', sans-serif;
+  font-size: 20px;
+  font-weight: 400;
+  font-style: italic;
+  color: #81a9d4;
+}
+.side-wrapper{
+  width: 700px;
+  margin-top: 25px;
+}
+.task-list-wrapper{
+  list-style: none;
+}
+.task-list--item{
+  display: flex;
+  align-items: center;
+  width: 700px;
+  height: 55px;
+  border-bottom: 1px solid #81a9d4;
+}
+.checkbox-item{
+  border-right: 1px solid #81a9d4;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 50px;
+  height: 100%;
+}
+.checkbox-switch{
+  cursor: pointer;
+  border: #81a9d4;
+  height: 20px;
+  width: 20px;
+}
 </style>
